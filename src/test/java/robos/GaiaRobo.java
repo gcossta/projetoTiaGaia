89fe88cf97b5d18aad309f10113e55e57f1db2e7
gcossta <gabriel.cossta@gmail.com>
2021-12-24 15:42:53 -0400

    Initial commit

---
+++ src/test/java/robos/GaiaRobo.java
@@ -0,0 +1,13 @@
+package robos;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class GaiaRobo {
+    public void fazerLogin() {
+        System.out.println("Vou fazer login.");
+    }
+
+    public void lancarNota() {
+        System.out.println("Vou lançar nota.");
+    }
+}
