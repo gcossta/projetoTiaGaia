d81addd5958b9661ff9077200a6e620a586a32e8
gcossta <gabriel.cossta@gmail.com>
2021-12-24 16:37:06 -0400

    Abrindo o Chrome com uso da Programação.

---
+++ src/test/java/executores/Executora.java
@@ -0,0 +1,12 @@
+package executores;
+
+import robos.GaiaRobo;
+import robos.JonasRobo;
+
+public class Executora {
+    public static void main(String[] args) {
+        GaiaRobo umaGaiaRobo = new GaiaRobo();
+
+        umaGaiaRobo.abrirChome();
+    }
+}
--- src/test/java/robos/GaiaRobo.java
+++ src/test/java/robos/GaiaRobo.java
@@ -1,8 +1,16 @@
 package robos;
 
-import jdk.swing.interop.SwingInterOpUtils;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GaiaRobo {
+    public void abrirChome() {
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver julioDriver = new ChromeDriver();
+
+        julioDriver.get("http://juliodelima.com.br/taskit");
+    }
+
     public void fazerLogin() {
         System.out.println("Vou fazer login.");
     }
